9c7085be0a18ee881c9c9f2b0bb53d86f1696684
qchehuang <hhuan10@126.com>
2018-05-13 11:55:10 +0800

    modify the code
    1). deal with case with invalid digitis
    2).deal with case with negative starts of digitis

--- _atoi/atoi.py
+++ _atoi/atoi.py
@@ -5,25 +5,34 @@ import math
 def atoi(str):
 
     len_str = len(str) -1
-
     value = 0
+    negative = False
 
     for inter in str:
-        print(inter)
-        if inter not in ['0','1','2','3','4','5','6','7','8','9']:
-            print("Invalid input for the literal for int()")
-            return 'Invalid'
+        if inter is '-':
+            len_str = len_str - 1
+            negative = True
+            continue
+        else:
+            if inter not in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+                print("invalid input for the literal for int")
+                return 'invalid'
+            else:
+                value += int(inter)*int(math.pow(10,len_str))
+                len_str = len_str - 1
 
-        value += int(inter) * int(math.pow(10,len_str))
-        len_str = len_str - 1
+    if negative is True:
+        return -1 * value
+    else:
+        return value
 
-    return value
 
 if __name__ == "__main__":
 
-    #last_value1 = atoi('123456')
-    last_value2 = atoi('x12345')
+    return_value1 = atoi('123456')
+    return_value2 = atoi('x12345')
+    return_value3 = atoi('-12345')
 
-    #print(last_value1)
-
-    print(last_value2)
+    print("value1 = ", return_value1)
+    print("value2 = ", return_value2)
+    print("value3 = ", return_value3)
